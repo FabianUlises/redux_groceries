bccf83cd7c4e1a4a973c00c3bb7440a919b86bed
FabianUlises <ulisesfab.car@outlook.com>
2023-03-20 05:59:01 -0700

    Set dom elements to variables and instantiated state

---
+++ script.js
@@ -0,0 +1,8 @@
+// Establish DOM elements as variables
+const grocerySubmit = document.querySelector('#addGrocery');
+const list = document.querySelector('#list');
+const clearBtn = document.querySelector('#clear-btn');
+// Default state value
+const initialState = {
+    groceries: []
+};
